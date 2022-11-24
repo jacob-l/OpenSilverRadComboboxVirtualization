6eae930f76d8eb67a22fe0938daae7d7861fdf81
Iakov Lilo <jlilo@hwdtech.com>
2022-11-24 13:43:15 +1100

    Add project files.

---
+++ OpenSilverRadComboboxVirtualization/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Telerik.Windows.Controls;
+
+namespace OpenSilverRadComboboxVirtualization
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+            var languages = new List<Language>
+            {
+                new Language { ID = 1, DisplayName = "English" },
+                new Language { ID = 2, DisplayName = "French" },
+            };
+
+            for (var i = 0; i < 1; i++)
+            {
+                languages.Add(new Language
+                {
+                    ID = i + 2,
+                    DisplayName = "Language" + i
+                });
+            }
+            this.Rcb.ItemsSource = languages;
+        }
+    }
+
+    public class Language
+    {
+        public int ID { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
